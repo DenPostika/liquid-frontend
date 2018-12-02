0dd237a43f028f7e1ecc1259d2de4bcbb32d0b49
den.postika <den.postika@gmail.com>
2018-12-02 21:46:59 +0200

    Add ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
+import Icon from './svg/Icon';
+import KHAILogo from './svg/KHAILogo';
+import Device from './svg/Device';
+import QR from './svg/QR';
+import Flash from './svg/Flash';
+import WarningIcon from './svg/WarningIcon';
+import ChartistGraph from 'react-chartist';
+
+const API_URL = 'http://localhost:8000';
 
 class App extends Component {
+  state = {
+    online: null,
+    warning: null,
+    consumptions: {},
+    left: 100,
+  };
+
+  componentDidMount() {
+    this.getOnlineStatus();
+    this.getWarningStatus();
+    this.getConsumptions();
+
+    setInterval(() => {
+      this.getOnlineStatus();
+      this.getWarningStatus();
+      this.getConsumptions();
+    }, 10 * 1000);
+  }
+
+  getOnlineStatus = () => {
+    axios.get(API_URL + '/online').then(res => {
+      const { data } = res;
+      const timeInSec =
+        new Date().getTime() / 1000 - new Date(data.date).getTime() / 1000;
+      const minutes = timeInSec / 60;
+
+      if (minutes <= 5) {
+        this.setState({
+          online: true,
+        });
+      } else {
+        this.setState({
+          online: false,
+        });
+      }
+    });
+  };
+
+  getWarningStatus = () => {
+    axios.get(API_URL + '/warning').then(res => {
+      const { data } = res;
+
+      if (data) {
+        if (data.status !== 'OK') {
+          this.setState({
+            warning: data.status,
+          });
+        }
+      } else {
+        this.setState({
+          warning: null,
+        });
+      }
+    });
+  };
+
+  getConsumptions = () => {
+    axios.get(API_URL + '/consumptions').then(res => {
+      const { data } = res;
+      const sorted = {};
+
+      if (data) {
+        for (let i = 0; i < data.length; i += 1) {
+          const date = new Date(data[i].date);
+
+          const dd = date.getDate();
+          const mm = date.getMonth() + 1;
+          const yyyy = date.getFullYear();
+
+          if (sorted[`${dd}/${mm}/${yyyy}`]) {
+            sorted[`${dd}/${mm}/${yyyy}`].poured += +data[i].poured;
+          } else {
+            sorted[`${dd}/${mm}/${yyyy}`] = {
+              poured: +data[i].poured,
+              date: `${dd}/${mm}/${yyyy}`,
+            };
+          }
+        }
+
+        this.setState({
+          consumptions: sorted,
+          left: data[data.length - 1].water_left * 100 / 19,
+        });
+      }
+    });
+  };
+
+  getLabels = () => {
+    const { consumptions } = this.state;
+    const labels = [];
+    for (let key in consumptions) {
+      labels.push(key);
+    }
+
+    return labels;
+  };
+
+  getSeries = () => {
+    const { consumptions } = this.state;
+    const series = [];
+
+    for (let key in consumptions) {
+      series.push(consumptions[key].poured);
+    }
+
+    return series;
+  };
+  
   render() {
+    const { online, warning, left } = this.state;
+
+    const simpleLineChartData = {
+      labels: this.getLabels(),
+      series: [this.getSeries()],
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="wrapper">
+        <header>
+          <Icon />
+          <h1>LIQUID</h1>
         </header>
+        <div className="content">
+          <div className="container">
+            <Device percent={left} />
+          </div>
+          <div className="container">
+            {online !== null ? (
+              <span>
+                <Flash color={online ? '#52D471' : '#d4343b'} />{' '}
+                {online ? 'ONLINE' : 'OFFLINE'}
+              </span>
+            ) : null}
+            {warning !== null ? (
+              <span>
+                <WarningIcon color={'#D8D94A'} /> {warning}
+              </span>
+            ) : null}
+            <span className="chart">
+              <ChartistGraph data={simpleLineChartData} type={'Line'} />
+            </span>
+          </div>
+          <div className="container">
+            <QR />
+          </div>
+        </div>
+        <footer>
+          <KHAILogo />
+          <span>© Denis Postyka & Yuriy Teslenko</span>
+        </footer>
       </div>
     );
   }
---
+++ src/svg/Icon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Icon extends React.Component {
+  render() {
+    return (
+        <svg width='31' height='44' viewBox='0 0 31 44' xmlns='http://www.w3.org/2000/svg'>
+            <g id='Page-1' fill='none' fillRule='nonzero'>
+                <path d='M15.6266987,3.67726143 C32.8328655,29.031722 32.8328655,41.7089522 15.6266987,41.7089522 C-1.57946818,41.7089522 -1.57946818,29.031722 15.6266987,3.67726143 Z'
+                      id='Path-10' stroke='#FFF' strokeWidth='4' />
+                <path d='M15.4551086,10.6079161 C28.0747923,29.2038808 28.0747923,38.5018631 15.4551086,38.5018631 C2.83542504,38.5018631 2.83542504,29.2038808 15.4551086,10.6079161 Z'
+                      id='Path-10' fill='#69D0EF' />
+            </g>
+        </svg>
+    );
+  }
+}
+
+export default Icon;
